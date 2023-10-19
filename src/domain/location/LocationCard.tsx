1218e538566b4c59b0c7d5112731051ad6ca25a2
Atmeshwar <atmeshwar.singh@classicinformatics.com>
2023-10-19 15:54:00 +0530

    refactor: add link to location card

--- src/domain/location/LocationCard.tsx
+++ src/domain/location/LocationCard.tsx
@@ -1,5 +1,6 @@
 import { hoursSinceUpdate } from "utils/hoursSinceUpdate";
 import { Location } from "__generated__/graphql";
+import { Link } from "react-router-dom";
 
 type LocationCardProps = Location;
 
@@ -8,17 +9,22 @@ export function LocationCard({
   address,
   status,
   updatedAt,
+  id,
 }: LocationCardProps) {
   return (
-    <div className="border border-slate-500 bg-white shadow-sm rounded-md p-4 grid grid-cols-[2fr_1fr] gap-3">
-      <h3 className="font-semibold text-lg col-start-1 row-start-1">{name}</h3>
-      <p className="col-span-2 col-start-1 row-start-2">{address ?? "N/A"}</p>
-      <span className="col-start-2 row-start-1 rounded-full p-3 bg-amber-300">
-        {status ?? "N/A"}
-      </span>
-      <span className="col-start-2 row-start-2 text-gray-400">
-        {hoursSinceUpdate(updatedAt)}
-      </span>
-    </div>
+    <Link to={`/${id}`}>
+      <div className="border border-slate-500 bg-white shadow-sm rounded-md p-4 grid grid-cols-[2fr_1fr] gap-3">
+        <h3 className="font-semibold text-lg col-start-1 row-start-1">
+          {name}
+        </h3>
+        <p className="col-span-2 col-start-1 row-start-2">{address ?? "N/A"}</p>
+        <span className="col-start-2 row-start-1 rounded-full p-3 bg-amber-300">
+          {status ?? "N/A"}
+        </span>
+        <span className="col-start-2 row-start-2 text-gray-400">
+          {hoursSinceUpdate(updatedAt)}
+        </span>
+      </div>
+    </Link>
   );
 }
