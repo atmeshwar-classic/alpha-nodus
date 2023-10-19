17f10a47eb58c43821ad4cbf586d6bc032010759
Atmeshwar <atmeshwar.singh@classicinformatics.com>
2023-10-19 15:26:06 +0530

    feat: add location card to page

--- src/domain/location/page.tsx
+++ src/domain/location/page.tsx
@@ -9,8 +9,50 @@ import {
   PlusIcon,
   MagnifyingGlassIcon,
 } from "@radix-ui/react-icons";
+import { useGetLocations } from "./useGetLocations";
+import { Skeleton } from "components/ui/Skeleton";
+import { LocationCard } from "./LocationCard";
 
 export function LocationsPage() {
+  const { data, loading, error } = useGetLocations();
+
+  function Content_() {
+    if (loading) {
+      return (
+        <div className="my-4 space-y-3">
+          <Skeleton className="w-full h-20" />
+          <Skeleton className="w-full h-20" />
+          <Skeleton className="w-full h-20" />
+          <Skeleton className="w-full h-20" />
+          <Skeleton className="w-full h-20" />
+        </div>
+      );
+    }
+
+    if (error) {
+      return (
+        <div className="my-4 space-y-2 text-center">
+          <h2 className="text-xl font-semibold">Something went wrong!</h2>
+          <p className="text-red-600">{error.message}</p>
+        </div>
+      );
+    }
+
+    return data?.locationList?.resources?.map((l) =>
+      l ? (
+        <LocationCard
+          key={l.id}
+          id={l.id}
+          name={l.name}
+          address={l.address}
+          type={l.type}
+          updatedAt={l.updatedAt}
+          status={l.status}
+        />
+      ) : null
+    );
+  }
+
   return (
     <main className="max-w-3xl mx-auto p-4 space-y-3">
       <LocationHeader>
@@ -27,6 +69,8 @@ export function LocationsPage() {
         placeholder="Search"
         prefixIcon={<MagnifyingGlassIcon className="h-5 w-5" />}
       />
+
+      <Content_ />
     </main>
   );
 }
