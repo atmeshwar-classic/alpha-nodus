d58e669b9f129fe73d8da2214205ede8de7db65e
Atmeshwar <atmeshwar.singh@classicinformatics.com>
2023-10-19 12:05:29 +0530

    feat: add location header to page

--- src/domain/location/page.tsx
+++ src/domain/location/page.tsx
@@ -1,3 +1,22 @@
+import {
+  LocationHeader,
+  LocationHeaderAction,
+  LocationHeaderTitle,
+} from "./LocationHeader";
+import { SymbolIcon, PlusIcon } from "@radix-ui/react-icons";
+
 export function LocationsPage() {
-  return <div>Locations page</div>;
+  return (
+    <main className="max-w-3xl mx-auto">
+      <LocationHeader>
+        <LocationHeaderAction className="px-10 justify-self-start">
+          <SymbolIcon />
+        </LocationHeaderAction>
+        <LocationHeaderTitle>Locations</LocationHeaderTitle>
+        <LocationHeaderAction variant="icon" className="justify-self-end">
+          <PlusIcon />
+        </LocationHeaderAction>
+      </LocationHeader>
+    </main>
+  );
 }
