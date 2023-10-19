45008e721c6de6c18bdb831546a78ea0210ec871
Atmeshwar <atmeshwar.singh@classicinformatics.com>
2023-10-19 11:43:53 +0530

    feat: add LocationHeader component

---
+++ src/domain/location/LocationHeader.tsx
@@ -0,0 +1,40 @@
+import { Button, type ButtonProps } from "components/ui/Button";
+import { ReactNode } from "react";
+import { cn } from "utils/cn";
+
+function LocationHeader({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <header className={cn("grid grid-cols-3 p-2", className)}>
+      {children}
+    </header>
+  );
+}
+
+function LocationHeaderTitle({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <h1 className={cn("text-lg font-semibold text-center", className)}>
+      {children}
+    </h1>
+  );
+}
+
+function LocationHeaderAction({
+  children,
+  ...rest
+}: { children: ReactNode } & ButtonProps) {
+  return <Button {...rest}>{children}</Button>;
+}
+
+export { LocationHeader, LocationHeaderAction, LocationHeaderTitle };
