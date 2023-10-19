8438c63b9b7a3a2f50b7f387d00d3fae8673b3d7
Atmeshwar <atmeshwar.singh@classicinformatics.com>
2023-10-19 12:34:39 +0530

    feat: add Search bar to locations page

--- src/domain/location/page.tsx
+++ src/domain/location/page.tsx
@@ -1,13 +1,18 @@
+import { Input } from "components/ui/Input";
 import {
   LocationHeader,
   LocationHeaderAction,
   LocationHeaderTitle,
 } from "./LocationHeader";
-import { SymbolIcon, PlusIcon } from "@radix-ui/react-icons";
+import {
+  SymbolIcon,
+  PlusIcon,
+  MagnifyingGlassIcon,
+} from "@radix-ui/react-icons";
 
 export function LocationsPage() {
   return (
-    <main className="max-w-3xl mx-auto">
+    <main className="max-w-3xl mx-auto p-4 space-y-3">
       <LocationHeader>
         <LocationHeaderAction className="px-10 justify-self-start">
           <SymbolIcon />
@@ -17,6 +22,11 @@ export function LocationsPage() {
           <PlusIcon />
         </LocationHeaderAction>
       </LocationHeader>
+
+      <Input
+        placeholder="Search"
+        prefixIcon={<MagnifyingGlassIcon className="h-5 w-5" />}
+      />
     </main>
   );
 }
